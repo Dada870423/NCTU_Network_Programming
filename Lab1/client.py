a0adcca06a59d7350bc7acd10287ed14463621de
tingda870423 <tingda870423@linux2.cs.nctu.edu.tw>
2020-03-19 00:48:27 +0800

    client no raw_input

--- Lab1/client.py
+++ Lab1/client.py
@@ -7,7 +7,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
 while True:
-    cmd = raw_input("Please input msg:")
+    cmd = input("Please input msg:")
     s.send(cmd)
     data = s.recv(1024)
     print ("server send :  "  (data))
