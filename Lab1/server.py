5ed43b2c1abfab92f29ec1b2cae73c824fe9e153
Ubuntu <ubuntu@ip-172-31-43-57.ec2.internal>
2020-03-18 16:33:56 +0000

    to python3

--- Lab1/server.py
+++ Lab1/server.py
@@ -9,14 +9,14 @@ server.bind((bind_ip,bind_port))
 
 server.listen(11)
 
-print "[*] Listening on %s:%d " % (bind_ip,bind_port)
+print ("[*] Listening on %s:%d ",   (bind_ip,bind_port))
 
 while True:
     client,addr = server.accept()
-    print 'Connected by ', addr
+    print ('Connected by ', addr)
 
     while True:
         data = client.recv(1024)
-        print "Client recv data : %s " % (data)
+        print ("Client recv data : %s ",  (data))
 
         client.send("ACK!")
