3b12c9deee174b3fbbc53f2528468ca8fcd06aec
dada <dada0423.cs05@nctu.edu.tw>
2020-03-20 20:03:59 +0000

    multithread done

--- Lab1/server.py
+++ Lab1/server.py
@@ -3,17 +3,17 @@ import _thread
 import sys
 
 def Client_Work(ClientSocket, addr):
-	msg = "Welcome to my BBS server\r\n"
-	ClientSocket.send(msg.encode('utf-8'))
-	ClientSocket.recv(1024)
-	while True:
-		msg_input = ""
-		if msg_input != "":
-			msg = "% "
-			ClientSocket.send(msg.encode('utf-8'))
-		msg_input = ClientSocket.recv(1024).decode('utf-8')
-		msg_input = msg_input.replace('\n', '').replace('\r', '')
-		print(msg_input)
+    msg = "Welcome to my BBS server\r\n"
+    ClientSocket.send(msg.encode('utf-8'))
+    ClientSocket.recv(1024)
+    msg_input = ""
+    while True:
+        if msg_input != "":
+            msg = "% "
+            ClientSocket.send(msg.encode('utf-8'))
+        msg_input = ClientSocket.recv(1024).decode('utf-8')
+        msg_input = msg_input.replace('\n', '').replace('\r', '')
+        print(msg_input)
 		
 
 bind_ip = "0.0.0.0"
@@ -23,10 +23,10 @@ server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
 server.bind((bind_ip,bind_port))
 
-server.listen(11)
+server.listen(17)
 print ("[*] Listening on  ", bind_ip,  bind_port)
 
 while True:
     client,addr = server.accept()
     print ("New Client connection :", addr)
-    _thread.start_new_thread(Client_Work(server, addr))
+    _thread.start_new_thread(Client_Work, (client, addr))
