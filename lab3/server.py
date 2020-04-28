a32468baa6c8fa1088a8db3fe293b4df5091c7ad
dada <dada0423.cs05@g2.nctu.edu.tw>
2020-04-28 15:45:37 +0800

    bug fix list-mail

--- lab3/server.py
+++ lab3/server.py
@@ -404,7 +404,7 @@ def Client_Work(ClientSocket, addr):
             else:
                 print("list")
                 msg_output = "DATA "
-                for row in c.execute("SELECT * FROM MAILS WHERE MIAL.Receiver=?", (login, )):
+                for row in c.execute("SELECT * FROM MAILS WHERE MAIL.Receiver=?", (login, )):
                     print("{:>5} {:^20} {:^20} {:^9}".format(row[5], row[1], row[2], row[3]))
                     msg_output = msg_output + "{:>7} {:^20} {:^20} {:^9}\r\n\r\n".format(row[0], row[1], row[2], row[3])
                 SEND(CMD = msg_output)
